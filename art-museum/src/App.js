9b2018091f42fb766e4aaa295c80fb17c6e50626
Manel-Os <oulds.manel@gmail.com>
2023-07-07 16:48:41 -0400

    Phase 3 completed

--- art-museum/src/App.js
+++ art-museum/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import harvardArt from './data/harvardArt';
 import GalleryNavigation from './components/GalleryNavigation';
 import GalleryView from './components/GalleryView';
-import {BrowserRouter as Router, Route} from "react-router-dom"
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 
 
 function App() {
@@ -14,9 +14,26 @@ function App() {
         <GalleryNavigation galleries={harvardArt} />
       </header>
       
-      <Route path="/galleries/:galleryId">
-          <GalleryView galleries={harvardArt} /> 
-      </Route>
+    <Switch>
+              <Route path="/galleries/:galleryId">
+                  <GalleryView galleries={harvardArt} /> 
+              </Route> 
+              <Route path= "/error">
+                  <h2>Error 404 Page not Found</h2>
+              </Route>
+              <Route exact path="/">
+                  <h2> Harvard Art Museum</h2>
+                  <p>Look but don not touch!. Please select the gallery in the navigation bar</p>
+              </Route>
+                <Route>
+                    <h2>Error 404 Page not Found</h2>
+                </Route>
+              
+    </Switch>
+      
+
+
+      
     </div>
 
   );
